9b490be2c24482e80232efed0b414f83676eaefb
prox <chank1012@naver.com>
2015-04-20 19:36:48 +0900

    some tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,28 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'not actually a test' do
+    assert_not false
+  end
+
+  test '#signup_success' do
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse(response.body)
+    assert json['user_name'] == 'testuser'
+    assert json['login_count'] == 1
+  end
+
+  test '3signup_failure_user' do
+    post :signup, {'username' => 'test', 'password' => 'testpass'}
+    json = JSON.parse(response.body)
+    assert json['error_code'] == -1
+  end
+
+  test '3signup_failure_pwd' do
+    post :signup, {'username' => 'testuser', 'password' => 'test'}
+    json = JSON.parse(response.body)
+    assert json['error_code'] == -2
+  end
+
 end
